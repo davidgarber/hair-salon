16fb125e6822d41da0cccd9b5ec3fab36e97488f
David Garber <davidgarber@gmail.com>
2015-05-08 11:43:06 -0700

    it is the same client if it is the same name and same client id

--- lib/client.rb
+++ lib/client.rb
@@ -1,19 +1,29 @@
 class Client
-  attr_reader(:name, :id)
+  attr_reader(:name, :client_id)
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
-    @id = attributes.fetch(:id)
+    @client_id = attributes.fetch(:client_id)
   end
 
   define_singleton_method(:all) do
-    returned_clients = DB.exec("SELECT * FROM clients")
+    returned_clients = DB.exec("SELECT * FROM clients;")
     clients = []
     returned_clients.each do |client|
       name = client.fetch("name")
-      id = client.fetch("id")
-      clients.push(Client.new({:name => name, :id => id}))
+      client_id = client.fetch("client_id").to_i()
+      clients.push(Client.new({:name => name, :client_id => client_id}))
     end
     clients
   end
+
+  define_method(:==) do |other_client|
+    self.name().==(other_client.name()).&(self.client_id().==(other_client.client_id()))
+  end
+
+  # define_method(:save) do
+  #   DB.exec("INSERT INTO clients (name, client_id) VALUES ('#{@name}', #{@client_id});")
+  # end
+
+
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -4,7 +4,7 @@ describe(Client) do
 
   describe("#name") do
     it('returns the name of the client') do
-      test_client = Client.new({:name => 'Julie', :id => 1})
+      test_client = Client.new({:name => 'Julie', :client_id => 1})
       expect(test_client.name()).to(eq('Julie'))
     end
   end
@@ -14,4 +14,29 @@ describe(Client) do
       expect(Client.all()).to(eq([]))
     end
   end
+
+
+  describe("#==") do
+    it("is the same client if it is the same name and client_id") do
+      client1 = Client.new({:name => "Julie", :client_id => 1})
+      client2 = Client.new({:name => "Julie", :client_id => 1})
+      expect(client1).to(eq(client2))
+    end
+  end
+
+  # describe("#save") do
+  #   it("adds a client to the array of saved clients") do
+  #     test_client = Client.new({:name => "Julie", :client_id => 1})
+  #     test_client.save()
+  #     expect(Client.all()).to(eq([test_client]))
+  #   end
+  # end
+
+  describe("#client_id") do
+    it("lets you read the client ID out") do
+      test_client = Client.new({:name => "Julie", :client_id => 1})
+      expect(test_client.client_id).to(eq(1))
+    end
+  end
+
 end
